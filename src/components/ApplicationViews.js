db5053083114cabf983b43a908ef86366568854a
Dillon Draper <dillonbdraper@gmail.com>
2020-11-30 11:53:02 -0600

    Added basic routing functionality to the application.  Routes return empty for now

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+
+export const ApplicationViews = (props) => {
+    return (
+        <>
+            <Route exact path="/">
+                <></>
+            </Route>
+
+            <Route exact path="/retailers">
+                <></>
+            </Route>
+
+            <Route exact path="/distributors">
+                <></>
+            </Route>
+        </>
+
+    )
+    }
